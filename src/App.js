67dbbcea1715e5ff3b719c83f9216276ac66383b
sahilvyas29 <vyassahil8433@gmail.com>
2023-05-16 11:41:30 +0530

    Chat app react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import Message from "./Components/Message";
+import { app } from "./firebase";
+import { useEffect, useState } from "react";
+
+import {
+  Box,
+  Button,
+  Container,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  onAuthStateChanged,
+   signOut, 
+   GoogleAuthProvider, 
+   getAuth, 
+   signInWithPopup} from 'firebase/auth';
+
+import {getFirestore, addDoc, collection}
+from 'firebase/firestore';
+
+const auth =getAuth(app);
+const db = getFirestore(app);
+
+const loginHandler = ()=>{
+
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+}
+
+const logoutHandler =()=> signOut(auth); 
+
+
+
 
 function App() {
+  const [user,setUser] = useState(false);
+
+
+  const submitHandler = async(e) =>{
+    e.prevantDefault();
+  
+    try{
+  
+      await addDoc(collection(db,"Messages"),{
+        text: "ADAS",
+        uid: user.uid,
+        
+      })
+    }
+  
+    catch(error){
+      alert(error)
+    }
+  }
+  
+
+  useEffect(()=>{
+   const unsubscribe =  onAuthStateChanged(auth,(data)=>{console.log(data)
+    setUser(data)
+  });
+
+  return ()=>{
+    unsubscribe();
+  };
+});
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={"red.50"}>
+      {
+        user?(
+          <Container h={"100vh"} bg={"white"}>
+        <VStack h={"full"} paddingY={"4"}>
+          <Button onClick={logoutHandler} w={"full"} colorScheme={"red"}>
+            Logout
+          </Button>
+          <VStack h="full" w="full" overflowY={"auto"}>
+            <Message text={"sample"} />
+            <Message text={"sample"} user={"me"} />
+          </VStack>
+
+          <form onSubmit={submitHandler} style={{ width: "100%" }}>
+            <HStack>
+              <Input placeholder="Enter a message" />
+              <Input />
+              <Button colorScheme={"purple"} type="submit">
+                {" "}
+                send{" "}
+              </Button>
+            </HStack>
+          </form>
+        </VStack>
+      </Container>
+
+        ) : <VStack bg="white" justifyContent={"center"} h="100vh">
+          <Button onClick={loginHandler} colorScheme={"purple"}>Sign In with Google</Button>
+        </VStack>
+      }
+          </Box>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTQmZlNO1YIcw7-DQyj-vTZdq9m2XExAw",
+  authDomain: "chatapp-fcb96.firebaseapp.com",
+  projectId: "chatapp-fcb96",
+  storageBucket: "chatapp-fcb96.appspot.com",
+  messagingSenderId: "462799847181",
+  appId: "1:462799847181:web:c583e6e7d412b179fe7a44"
+};
+
+
+export const app = initializeApp(firebaseConfig);
